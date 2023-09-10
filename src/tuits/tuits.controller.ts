9666d966de5cf85628a83a0af7cbcaf801417d9f
ManuelFBS <manuelf.borrego@gmail.com>
2023-09-10 15:50:29 -0400

    Creac. servicio tuits. Act, app.module.ts

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,10 +1,27 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Patch, 
+  Delete, 
+  Param, 
+  Query, 
+  Body, 
+  HttpCode, 
+  HttpStatus } from '@nestjs/common';
 
 @Controller('tuits')
 export class TuitsController {
   @Get()
   getTuits(): string {
     return 'Hello from Tuitter !!!';
+  }
+
+  @Get('/ord')
+  getTuitsOther(@Query() filterQuery): string {
+    const {searchTerm, orderBy} = filterQuery
+
+    return `All ${ searchTerm} tuits ordered by ${orderBy}`
   }
 
 //   @Get('/:id')     // tuits/1...
@@ -14,5 +31,21 @@ export class TuitsController {
   @Get('/:id')
   getTuit(@Param('id') id: string): string {
     return`Your tuit id is: ${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  createTuit(@Body('message') message: string): string {
+    return `Your tuit was: ${message}`
+  }
+
+  @Patch('/:id')
+  updateTuit(@Param('id') id: string, @Body() tuit) {
+    return `The tuit id: ${id} has been updated...!`
+  }
+
+  @Delete('/:id')
+  deleteTuit(@Param('id') id: string) {
+    return `The tuit id: ${id} has been deleted...!`
   }
 }
