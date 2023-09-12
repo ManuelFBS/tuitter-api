8f05cbba9b622d6660caa2d832a95f58137a1271
ManuelFBS <manuelf.borrego@gmail.com>
2023-09-12 11:30:59 -0400

    Act. app.module.ts. Elimin.  archivos innecesarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TuitsController } from './modules/tuits/tuits.controller';
-import { TuitsService } from './modules/tuits/tuits.service';
 import { TuitsModule } from './modules/tuits/tuits.module';
 
 @Module({
   imports: [TuitsModule],
-  controllers: [AppController, TuitsController],
-  providers: [AppService, TuitsService],
 })
 export class AppModule {}
