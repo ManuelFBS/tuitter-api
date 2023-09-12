3ce32d74e27217dbc1aeefb856bd292785038728
ManuelFBS <manuelf.borrego@gmail.com>
2023-09-12 11:07:14 -0400

    Creac. de tuits.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TuitsController } from './tuits/tuits.controller';
 import { TuitsService } from './tuits/tuits.service';
+import { TuitsModule } from './modules/tuits/tuits.module';
 
 @Module({
-  imports: [],
+  imports: [TuitsModule],
   controllers: [AppController, TuitsController],
   providers: [AppService, TuitsService],
 })
